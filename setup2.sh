a8c9acb7107f3f6abaf1732da342826f2dfd9f93
Sui Zhiyuan <suizhiyuan021435@gmail.com>
2023-12-03 11:26:11 +0800

    d1

--- setup2.sh
+++ setup2.sh
@@ -2,3 +2,10 @@
 # https://www.wireguard.com/install/
 
 # https://gitee.com/spoto/wireguard
+
+# https://gist.github.com/nealfennimore/92d571db63404e7ddfba660646ceaf0d
+
+netstat -rn
+
+
+traceroute www.google.com
